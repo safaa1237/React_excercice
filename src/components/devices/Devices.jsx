f9d8cb36f17bb5e158cf21f5bf3f794f2696c1b2
safaa1237 <41582021+safaa1237@users.noreply.github.com>
2021-03-27 12:58:04 +0100

    using the library react-error-boundary

--- src/components/devices/Devices.jsx
+++ src/components/devices/Devices.jsx
@@ -1,29 +1,56 @@
-import React from 'react'
-import { Grid, Menu, Input } from 'semantic-ui-react'
+import React  , {useState} from 'react'
+import { Grid, Menu, Input, Image , Button} from 'semantic-ui-react'
 import { withRouter } from 'react-router-dom'
 
 import { seedDevices } from 'seedData/devices.json'
 import DeviceList from './DeviceList'
+import {ErrorBoundary} from 'react-error-boundary'
+import oops from "../../images/OOPSIN.png"
 
-const Devices = () => (
-  <Grid>
-      <Grid.Row>
-        <Menu position='right'>
-          <Menu.Item>
-            <Input
-              icon='search'
-              iconPosition='left'
-              placeholder='fake search :O'
-            />
-          </Menu.Item>
-        </Menu>
-      </Grid.Row>
+const Devices = () => {
+  
+  const [errorblockFlag, seterrorblockflag]=useState(false);
 
-      <Grid.Row>
-          <DeviceList devices={seedDevices} />        
-      </Grid.Row>
+  const showerrorBlock = (error, componentStack)=>{
+    if(errorblockFlag){
+      return <div>{error.message} :{componentStack}</div>
+    }
+  }
 
-  </Grid>
-)
+  function fallbackRender(error, resetErrorBoundary , componentStack){
+    return (
+      <div>
+        <Image src={oops}></Image>
+        <Button onClick={showError=()=>{ seterrorblockflag(true)}}>Show more details about error</Button>
+        <Button onClick={resetErrorBoundary}> Re-Try </Button>
+        {showerrorBlock(error, componentStack)}
+      </div>
+    )
+  }
+
+  return (
+    <Grid>
+      <ErrorBoundary fallbackRender={fallbackRender(error, resetErrorBoundary, componentStack)}>
+        
+        <Grid.Row>
+          <Menu position='right'>
+            <Menu.Item>
+              <Input
+                icon='search'
+                iconPosition='left'
+                placeholder='fake search :O'
+              />
+            </Menu.Item>
+          </Menu>
+        </Grid.Row>
+
+        <Grid.Row>   
+            <DeviceList devices={seedDevices} />        
+        </Grid.Row>
+
+      </ErrorBoundary>
+    </Grid>
+  )
+}
 
 export default withRouter(Devices)
